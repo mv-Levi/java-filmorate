235c2f32df3c503af32095d52b10085634c02689
Nikita <153514866+mv-Levi@users.noreply.github.com>
2024-07-09 15:23:08 +0300

    Создал модели данных, REST контроллеры и тесты

---
+++ main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * Film.
+ */
+@Data
+public class Film {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private int duration;
+}
