5aa9d4d8a293eb7cfdcb8205c3a32138d2dc88ad
Nikita <153514866+mv-Levi@users.noreply.github.com>
2024-07-17 12:18:33 +0300

    Добавил хранилища
    Вынесите хранение данных о фильмах и пользователях в отдельные классы

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface FilmStorage {
+    Film addFilm(Film film);
+    Film updateFilm(Film film);
+    Collection<Film> getAllFilms();
+    Optional<Film> getFilmById(long id);
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface UserStorage {
+    User addUser(User user);
+    User updateUser(User user);
+    Collection<User> getAllUsers();
+    Optional<User> getUserById(long id);
+}
