47f9fe729e7429123e6bf867c0c4e71ac5a857d8
Nikita <153514866+mv-Levi@users.noreply.github.com>
2024-08-05 16:44:01 +0300

    Дополнил класс
    Добавил логирование и собрал в одну группу исключения, которые обрабатывают BAD REQUEST

--- src/main/java/ru/yandex/practicum/filmorate/controller/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,32 +12,28 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<Map<String, String>> handleValidationException(ValidationException e) {
+    @ExceptionHandler({ValidationException.class, ConditionsNotMetException.class})
+    public ResponseEntity<Map<String, String>> handleBadRequestException(Exception e) {
+        log.error("Bad request exception: {}", e.getMessage(), e);
         Map<String, String> response = new HashMap<>();
         response.put("error", e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Map<String, String>> handleNotFoundException(NotFoundException e) {
+        log.error("Not found exception: {}", e.getMessage(), e);
         Map<String, String> response = new HashMap<>();
         response.put("error", e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(ConditionsNotMetException.class)
-    public ResponseEntity<Map<String, String>> handleConditionsNotMetException(ConditionsNotMetException e) {
-        Map<String, String> response = new HashMap<>();
-        response.put("error", e.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleException(Exception e) {
+        log.error("Internal server error: {}", e.getMessage(), e);
         Map<String, String> response = new HashMap<>();
         response.put("error", "Внутренняя ошибка сервера");
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
