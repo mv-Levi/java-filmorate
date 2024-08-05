b1db56b70932bcd45a7da6277804903528e46eaf
Nikita <153514866+mv-Levi@users.noreply.github.com>
2024-08-05 18:06:43 +0300

    Обновил классы
    Теперь уровень контроллера не зависит от уровня доступа к данным

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.util.Collection;
 import java.util.List;
@@ -14,56 +12,50 @@ import java.util.List;
 @RestController
 @RequestMapping("/films")
 @Slf4j
+@RequiredArgsConstructor
 public class FilmController {
-    private final FilmStorage filmStorage;
     private final FilmService filmService;
-
-    @Autowired
-    public FilmController(FilmStorage filmStorage, FilmService filmService) {
-        this.filmStorage = filmStorage;
-        this.filmService = filmService;
-    }
 
     @GetMapping
     public Collection<Film> findAll() {
         log.info("Получение всех фильмов");
-        return filmStorage.getAllFilms();
+        return filmService.getAll();
     }
 
     @PostMapping
     public Film create(@RequestBody Film film) {
         log.info("Создание нового фильма: {}", film);
-        return filmStorage.addFilm(film);
+        return filmService.add(film);
     }
 
     @PutMapping
     public Film update(@RequestBody Film newFilm) {
         log.info("Обновление фильма: {}", newFilm);
-        return filmStorage.updateFilm(newFilm);
+        return filmService.update(newFilm);
     }
 
     @PutMapping("{id}/like/{userId}")
-    public void addLike(@PathVariable long id, @PathVariable long userId) {
+    public void add(@PathVariable long id, @PathVariable long userId) {
         log.info("Пользователь с id: {} ставит лайк фильму с id: {}", userId, id);
         filmService.addLike(id, userId);
     }
 
     @DeleteMapping("{id}/like/{userId}")
-    public void removeLike(@PathVariable long id, @PathVariable long userId) {
+    public void remove(@PathVariable long id, @PathVariable long userId) {
         log.info("Пользователь с id: {} удаляет лайк с фильма с id: {}", userId, id);
         filmService.removeLike(id, userId);
     }
 
     @GetMapping("/{id}")
-    public Film getFilm(@PathVariable long id) {
+    public Film get(@PathVariable long id) {
         log.info("Получение фильма с id: {}", id);
-        return filmStorage.getFilmById(id).orElseThrow(() -> new NotFoundException("Фильм не найден"));
+        return filmService.getById(id);
     }
 
     @GetMapping("/popular")
-    public List<Film> getMostPopularFilms(@RequestParam(defaultValue = "10") int size) {
+    public List<Film> getMostPopular(@RequestParam(defaultValue = "10") int size) {
         log.info("Получение {} самых популярных фильмов", size);
-        return filmService.getMostPopularFilms(size);
+        return filmService.getMostPopular(size);
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.Collection;
 import java.util.List;
@@ -14,61 +12,54 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
-
-    private final UserStorage userStorage;
     private final UserService userService;
-
-    @Autowired
-    public UserController(UserStorage userStorage, UserService userService) {
-        this.userStorage = userStorage;
-        this.userService = userService;
-    }
 
     @GetMapping
     public Collection<User> findAll() {
         log.info("Получение всех пользователей");
-        return userStorage.getAllUsers();
+        return userService.getAll();
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
         log.info("Создание нового пользователя: {}", user);
-        return userStorage.addUser(user);
+        return userService.add(user);
     }
 
     @PutMapping
     public User update(@RequestBody User newUser) {
         log.info("Обновление пользователя: {}", newUser);
-        return userStorage.updateUser(newUser);
+        return userService.update(newUser);
     }
 
     @PutMapping("{id}/friends/{friendId}")
-    public void addFriend(@PathVariable long id, @PathVariable long friendId) {
+    public void add(@PathVariable long id, @PathVariable long friendId) {
         log.info("Пользователь с id: {} добавляет в друзья пользователя с id: {}", id, friendId);
         userService.addFriend(id, friendId);
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable long id) {
+    public User get(@PathVariable long id) {
         log.info("Получение пользователя с id: {}", id);
-        return userStorage.getUserById(id).orElseThrow(() -> new NotFoundException("Пользователь не найден"));
+        return userService.getById(id);
     }
 
     @DeleteMapping("{id}/friends/{friendId}")
-    public void removeFriend(@PathVariable long id, @PathVariable long friendId) {
+    public void remove(@PathVariable long id, @PathVariable long friendId) {
         log.info("Пользователь с id: {} удаляет из друзей пользователя с id: {}", id, friendId);
         userService.removeFriend(id, friendId);
     }
 
     @GetMapping("{id}/friends/common/{otherId}")
-    public List<User> getCommonFriends(@PathVariable long id, @PathVariable long otherId) {
+    public List<User> getCommon(@PathVariable long id, @PathVariable long otherId) {
         log.info("Получение списка общих друзей пользователя с id: {} и пользователя с id: {}", id, otherId);
         return userService.getCommonFriends(id, otherId);
     }
 
     @GetMapping("{id}/friends")
-    public List<User> getAllFriends(@PathVariable long id) {
+    public List<User> getAll(@PathVariable long id) {
         log.info("Получение списка друзей пользователя с id: {}", id);
         return userService.getAllFriends(id);
     }
