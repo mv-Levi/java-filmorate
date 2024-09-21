4fc84ec0b14222137878dbb9216cfa81e4790264
Nikita <153514866+mv-Levi@users.noreply.github.com>
2024-09-21 04:08:28 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
@@ -13,4 +14,12 @@ public interface FilmStorage {
     Collection<Film> getAll();
 
     Optional<Film> getById(long id);
+
+    void delete(long id);
+
+    List<Film> getPopular(int count);
+
+    // Методы для управления лайками
+    void likeFilm(long filmId, long userId);
+    void unlikeFilm(long filmId, long userId);
 }
